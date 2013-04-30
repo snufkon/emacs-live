ec868bc226df3ee4f0d8f7e6a600cbe78e4dd764
snufkon <snufkon@gmail.com>
2013-04-30 22:14:46 +0900

    update emacs-live.el

--- emacs-live.el
+++ emacs-live.el
@@ -1,2 +1,6 @@
 
+;;; live/power-pack の emacs-git-gutter のバージョンが古いため、dev/power-pack を利用する
+(live-ignore-packs '(live/power-pack))
+(live-add-packs '(dev/power-pack))
+
 (live-add-packs '(~/.emacs.d/packs/live/snufkon-pack))
