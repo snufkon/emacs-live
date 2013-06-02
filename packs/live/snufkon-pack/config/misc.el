cc41c0527743755de45466c53d1466b11ef40d99
snufkon <snufkon@gmail.com>
2013-06-03 01:10:33 +0900

    toggle-window-split を追加

--- packs/live/snufkon-pack/config/misc.el
+++ packs/live/snufkon-pack/config/misc.el
@@ -46,3 +46,31 @@
 ;;; 使い捨てファイル作成用
 (require 'open-junk-file)
 (setq open-junk-file-format "~/Code/junk/%Y-%m/%Y-%m-%d-%H%M%S.")
+
+
+;;; ウィンドウ分割の横、縦を切り替える
+;;; 参考: http://whattheemacsd.com/
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
